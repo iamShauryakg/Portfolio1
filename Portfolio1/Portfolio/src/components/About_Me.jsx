13a515e52593ef6b7d7cce70d9342a409c54012e
ShauryaBhartiya <iamshauryakgupta@gmail.com>
2025-07-17 16:50:45 +0530

    updated profile

--- Portfolio1/Portfolio/src/components/About_Me.jsx
+++ Portfolio1/Portfolio/src/components/About_Me.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import profile3 from '../assets/profile3.jpg'
+import aboutback from '../assets/aboutme.jpg'
 
 function About_Me() {
     useEffect(() => {
@@ -7,22 +8,25 @@ function About_Me() {
     })
 
   return (
-    <section id="about" className="about-me" >
-        <div className="aboutimg">
-            <div className="aboutme">
-                <div className="title">
+    <section id="about" className="h-screen w-[100%] bg-[#222d33]" >
+        <div className="bg-no-repeat bg-center bg-cover overflow-hidden h-screen"
+        style={{ backgroundImage: `url(${aboutback})` }}
+        >
+            <div className="relative flex h-[100%] w-full items-center flex-col ">
+                <div className="title w-auto py-[30px] text-[40px] font-medium text-[rgb(204,113,113)] relative">
                     <h2>About Me</h2>
+                    <span className="absolute left-0 bottom-0 h-[5px] w-[100%] bg-gradient-to-r from-[rgb(119,55,55)] to-[rgb(64,201,30)]"></span>
                 </div>
     
-                <div className="content2">
+                <div className="absolute flex h-[100vh] w-[100%] justify-around items-center">
                     <div className="image2">
                         <img src={profile3} alt="about me image" />
                     </div>
-                    <div className="aboutme-content">
+                    <div className="relative flex flex-col w-[50%] text-[18px] text-[#beb7b7]">
                         <div className="aname">I am Shaurya </div>
                         <div className="ajob"> Aspiring Software engineer </div>
                         <br/>
-                        <div className="motive-line">
+                        <div className="">
                             I am Computer engineering student. <br/>
                             i am very passionate and hardworking computer engineering student <br/>
                             lokking forward to became a skilled Software Engineer and serve my excelence to the company. <br/>
--- Portfolio1/Portfolio/src/components/header_part.jsx
+++ Portfolio1/Portfolio/src/components/header_part.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
 import About_Me from './About_Me';
+import background from '../assets/background.png';
 import  profile2 from '../assets/profile2.jpg'
 import  logo_shaurya from '../assets/logo-shaurya.png'
 
@@ -12,33 +13,50 @@ function Navbar() {
 
 
     return (
-       <header id="home" className="header">
-        <div className='headerimg'>
+       <header id="home" className="h-screen max-w-screen">
+        <div className='bg-no-repeat bg-center bg-cover overflow-hidden h-screen'
+             style={{ backgroundImage: `url(${background})` }}>
 
-        <nav>
-          <div className="menu">
-            <div className="logo">
-              <img src={logo_shaurya} alt="logo" />
+        <nav className="fixed py-[10px] flex bg-[rgba(0, 0, 0, 0.233)]  w-full z-[999]  mx-auto">
+          <div className="w-full m-auto flex items-center justify-around py-[10px] px-[30px]">
+            <div className="m-auto ">
+              <img src={logo_shaurya} alt="logo" className='w-[140px] h-auto' />
             </div>
+
             <div className="navbar">
-              <ul>
-                <li><a href="#home" >Home</a></li>
-                <li><a href="#about" >About</a></li>
-                <li><a href="#" >Education</a></li>
-                <li><a href="#" >Projects</a></li>
-                <li><a href="#" >Achievements</a></li>
-                <li><a href="#" >Resume</a></li>
-                <li><a href="#" >Contact Me</a></li>
+              <ul className="flex gap-7  mr-4">
+                <li className="ml-4 py-6">
+                  <a href="#home" className="px-4 py-4 text-[#fff] font-semibold text-[20px] hover:bg-white hover:text-black rounded transition duration-400">Home</a>
+                </li>
+                <li className="ml-4 py-3">
+                  <a href="#about" className="px-4 py-2  text-[#fff]  font-semibold text-[20px] hover:bg-white hover:text-black rounded transition duration-400">About</a>
+                </li>
+                <li className="ml-4 ">
+                  <a href="#education" className="px-4 py-2 text-[#fff] font-semibold text-[20px] hover:bg-white hover:text-black rounded transition duration-400">Education</a>
+                </li>
+                <li>
+                  <a href="#projects" className="px-4 py-2 text-[#fff] font-semibold text-[20px] hover:bg-white hover:text-black rounded transition duration-400">Projects</a>
+                </li>
+                <li>
+                  <a href="#achievements" className="px-4 py-2 text-[#fff] font-semibold text-[20px] hover:bg-white hover:text-black rounded transition duration-400">Achievements</a>
+                </li>
+                <li>
+                  <a href="#resume" className="px-4 py-2 text-[#fff] font-semibold text-[20px] hover:bg-white hover:text-black rounded transition duration-400">Resume</a>
+                </li>
+                <li>
+                  <a href="#contact" className="p-[50px] text-[#fff] font-semibold text-[20px] hover:bg-white hover:text-black rounded transition duration-400">Contact Me</a>
+                </li>
               </ul>
             </div>
+
           </div>
         </nav>
 
 
-    <div className="content12">
-      <div className="text">Hello,<span> It's me</span></div>
-      <div className="name">Shaurya Kumar Gupta</div>
-      <div className="role">
+    <div className="relative top-[33%] left-[5%] z-2 ">
+      <div className="text-[#fff] text-[25px] ">Hello,<span className='text-[rgb(209,49,49)] text-[27px]'> It's me</span></div>
+      <div className="text-[30px] text-[#e0dede]">Shaurya Kumar Gupta</div>
+      <div className="text-[40px] text-[#73bde2] flex my-0 mx-[10px]">
         <span>And, I'm a</span>
         <div className="typingtext">
         <span> Software Developer</span>
@@ -46,8 +64,8 @@ function Navbar() {
       </div>
     </div>
 
-    <div className="images">
-      <img src={profile2} alt="profile2" className="profile" />
+    <div className="w-[35%] h-[60%] absolute bottom-[140px] right-[150px]">
+      <img src={profile2} alt="profile2" className="images1" />
     </div>
 
     </div>
